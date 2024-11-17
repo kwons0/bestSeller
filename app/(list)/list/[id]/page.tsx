e053b5e4095f5de42c55b6297aa1b93f1690f026
ksy1128 <ksy1128@echomarketing.co.kr>
2024-11-17 20:40:39 +0900

    change code

--- app/(list)/list/[id]/page.tsx
+++ app/(list)/list/[id]/page.tsx
@@ -1,15 +1,16 @@
 import Category from "../../../../componenets/category";
+import { Metadata } from 'next';
 
 interface ImetaParams {
     params: {
         id: string;
-    }
+    };
 }
 
-export async function generateMetadata({params: {id}}: ImetaParams){
-    return{
-        title: `${id} | The New York Times`
-    }
+export async function generateMetadata({ params: { id } }: ImetaParams): Promise<Metadata> {
+    return {
+        title: `${id} | The New York Times`,
+    };
 }
 
 export default function Detail(){
